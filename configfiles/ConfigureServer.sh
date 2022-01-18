f65f196b10495fd7ee62beb0c808c07f67a156b1
XavierSJC <alexsantosxavier@gmail.com>
2022-01-18 00:09:18 -0300

    Removed template files

--- configfiles/ConfigureServer.sh
+++ configfiles/ConfigureServer.sh
@@ -10,17 +10,15 @@ then
     echo "Please use the environment variable SERVER_NAME to configure a name for your server"
     exit 1
 fi
-cp -f /home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini.template /home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini
-sed -i "s/Your_name_server/$SERVER_NAME/" /home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini
+sed -i "/ServerName=/c\ServerName=\"$SERVER_NAME\"" /home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini
 echo "Updated the name server to $SERVER_NAME"
 
 if [ "$RCON_PWD" != "ChangeThisPassword" ] 
 then
-    cp -f /home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt.template /home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt
-    sed -i "s/ChangeThisPassword/$RCON_PWD/" /home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt
-    sed -i "s/9100/$RCON_PORT/" /home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt
-
+    sed -i "/Password=/c\Password=$RCON_PWD" /home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt
     echo "Updated RCON password"
+
+    sed -i "/Port=/c\Port=$RCON_PORT" /home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt
     echo "Updated port RCON to $RCON_PORT"
 fi
 
