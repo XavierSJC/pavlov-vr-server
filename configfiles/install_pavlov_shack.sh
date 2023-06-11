5eb69ea15434350b9be229a5a971eea05cb9afea
Alex dos Santos Xavier <XavierSJC@users.noreply.github.com>
2023-06-11 01:14:23 -0300

    Created separated docker files and install_pavlov scripts to each Pavlov version (#17)

---
+++ configfiles/install_pavlov_shack.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -f "~/Steam/steamcmd.sh" ];
+then
+echo -e "Beginning Pavlov VR update run on $(date)\n\n"
+else
+  echo "Installing Steam client"
+  mkdir ~/Steam && cd ~/Steam && curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+  mkdir -p ~/.steam/sdk64
+fi
+
+echo "Installing Pavlov Steam"
+~/Steam/steamcmd.sh +force_install_dir /home/steam/pavlovserver +login anonymous +app_update 622970 -beta shack +exit
+chmod +x ~/pavlovserver/PavlovServer.sh
+
+echo "Updating Steam Client"
+~/Steam/steamcmd.sh +login anonymous +app_update 1007 +quit
+cp ~/Steam/steamapps/common/Steamworks\ SDK\ Redist/linux64/steamclient.so ~/.steam/sdk64/steamclient.so
+cp ~/Steam/steamapps/common/Steamworks\ SDK\ Redist/linux64/steamclient.so ~/pavlovserver/Pavlov/Binaries/Linux/steamclient.so
