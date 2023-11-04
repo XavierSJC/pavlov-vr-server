5ef5da18daaf0279dd8c5b44e8bc0a51582563d9
Alex Xavier <alexsantosxavier@gmail.com>
2023-11-04 20:04:08 -0300

    Fixed issues: 1. Send the same match multiple timese 2. Get new files for each loop

--- configfiles/ExportPavlovShackStats.sh
+++ configfiles/ExportPavlovShackStats.sh
@@ -26,7 +26,7 @@ fi
 
 if [ -d $StatsPath ]
 then
-	filesToExtract=$(find $StatsPath)
+	echo "Path '$($StatsPath)' is valid"
 else
 	echo "Folder does not exist $filesToExtract"
 	exit
@@ -44,42 +44,40 @@ fi
 while true
 do
 	echo "Starting exporting"
+	filesToExtract=$(find $StatsPath -maxdepth 1 -name '*.json')
 	for file in $filesToExtract
 	do
 		filename=$(basename $file)
 
-		if [[ $filename == *".json" ]]
-		then
-			echo "Exporting file $filename"
-			url=$endpoint/$filename
+		echo "$(date +"%d/%b/%Y %H:%M:%S") Exporting file $filename"
+		url=$endpoint/$filename
 
-			postResult=$(curl -k -X POST \
-			-s --write-out '%{http_code}' \
-			-H "Content-Type: application/json" \
-			--data @$file \
-			"$url")
-			
-			if [[ $postResult == "201" ]]
+		postResult=$(curl -k -X POST \
+		-s --write-out '%{http_code}' \
+		-H "Content-Type: application/json" \
+		--data @$file \
+		"$url")
+		
+		if [[ $postResult == "201" ]]
+		then
+			echo -e "\tFile sent successfully"
+			mv $file $OldStatsPath/$filename
+		else
+			echo -e "\tERROR in the POST operation to URL: $url"
+			echo -e "\thttp_code: $postResult"
+			if [[ $postResult == *"already exist"* ]]
 			then
-				echo -e "\tFile sent successfully"
+				echo -e "\tMatch already sent"
 				mv $file $OldStatsPath/$filename
+			elif [[ $postResult == *000* ]]
+			then
+				echo -e "\tCheck internet connection, this file will be exported again"
+			elif [[ $postResult == *404* ]]
+			then
+				echo -e "\tCheck internet connection, this file will be exported again"
 			else
-				echo -e "\tERROR in the POST operation to URL: $url"
-				echo -e "\thttp_code: $postResult"
-				if [[ $postResult == *"already exist"* ]]
-				then
-					echo -e "\tMatch already sent"
-					mv $file $OldStatsPath/$filename
-				elif [[ $postResult == *000* ]]
-				then
-					echo -e "\tCheck internet connection, this file will be exported again"
-				elif [[ $postResult == *404* ]]
-				then
-					echo -e "\tCheck internet connection, this file will be exported again"
-				else
-					echo -e "\tUnnkown issue, renaming file to avoid new issues"
-					mv $file $file.error
-				fi
+				echo -e "\tUnnkown issue, renaming file to avoid new issues"
+				mv $file $file.error
 			fi
 		fi
 	done
