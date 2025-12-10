1d5b2c830facaabd8c9ffe5264702760712f07f2
Alex dos Santos Xavier <XavierSJC@users.noreply.github.com>
2025-12-09 22:31:55 -0300

    Fixed issue when using docker-compose (#28)
    Minor improvements

--- configfiles/ConfigureServer.sh
+++ configfiles/ConfigureServer.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 echo "Configuring Pavlov Server"
-if [ "$UPDATE_SERVER" = "True" ] 
-then
+if [ "$UPDATE_SERVER" = "True" ]; then
     install_pavlov=$(find /home/steam/pavlovserver/ -name 'install_pavlov_*')
     $install_pavlov
 fi
 
-if [ "$SERVER_NAME" = "Your_name_server" ] 
-then
+if [ "$SERVER_NAME" = "Your_name_server" ]; then
     echo "Please use the environment variable SERVER_NAME to configure a name for your server"
     exit 1
 fi
 sed -i "/ServerName=\"Your_name_server\"/c\ServerName=\"$SERVER_NAME\"" /home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini
 echo "Updated the name server to $SERVER_NAME"
 
-if [ "$RCON_PWD" != "ChangeThisPassword" ] 
-then
+if [ "$RCON_PWD" != "ChangeThisPassword" ]; then
     sed -i "/Password=ChangeThisPassword/c\Password=$RCON_PWD" /home/steam/pavlovserver/Pavlov/Saved/Config/RconSettings.txt
     echo "Updated RCON password"
 
@@ -23,14 +20,12 @@ then
     echo "Updated port RCON to $RCON_PORT"
 fi
 
-if [ "$SHUFFLE_MAPS" = "True" ] 
-then
+if [ "$SHUFFLE_MAPS" = "True" ]; then
     echo "Shuffling map rotation"
     /home/steam/pavlovserver/shuffleMaps.sh /home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini
 fi
 
-if [ "$STATS_ENDPOINT" != "/api/PavlovShackStats/" ] 
-then
+if [ "$STATS_ENDPOINT" != "/api/PavlovShackStats/" ]; then
     echo "Starting export stats task"
     /home/steam/pavlovserver/ExportPavlovShackStats.sh $STATS_ENDPOINT >> /home/steam/pavlovserver/Pavlov/Saved/Logs/ExportPavlovShackStats.log &
 fi
